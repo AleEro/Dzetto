94defcffa59c7f11d4049b63c7e0b49dbc6f7758
yohoko <paradox.wolf@mail.ru>
2019-10-28 22:03:28 +0300

    new 2 files with parser library

---
+++ 2parser.py
@@ -0,0 +1,13 @@
+import yaml
+
+def decc(file_name):
+    with open(file_name, "r", encoding="utf_8") as stream:
+            match_str = stream.read()
+            a = yaml.load(match_str)
+            print(a)
+            for key, value in a['i_rus'].items():
+                #for i1,z1 in z:
+#                    print(z1)
+                print(key + ': ', value)
+            #print(len(a))
+decc(r'C:\Users\Z510\Desktop\sofe\sofe_technology_l_english — копия.yml')
