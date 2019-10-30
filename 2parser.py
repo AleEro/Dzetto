2f64331083a23bd5c80204da668fc79c46dab476
yohoko <paradox.wolf@mail.ru>
2019-10-30 20:57:58 +0300

    0.11

--- 2parser.py
+++ 2parser.py
@@ -4,13 +4,11 @@ import yaml
 def decc(file_name):
     with open(file_name, "r", encoding="utf-8") as stream:
             match_str = stream.read()
+
             a = yaml.load(match_str)
             print(a)
             for key, value in a.items():
-                #for i1,z1 in z:
-                #    print(z1)
                 print(str(key) + ': ', value)
-            #print(len(a))
 
 
 decc(r'C:\Users\Z510\Desktop\sofe\sofe_technology_l_english.yml')
