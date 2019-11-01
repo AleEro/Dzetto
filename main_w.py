c82c76a46ae3be04aae3eff2c4277ebdde6f0aaf
yohoko <paradox.wolf@mail.ru>
2019-11-01 20:23:54 +0300

    0.5

--- main_w.py
+++ main_w.py
@@ -11,6 +11,46 @@ from time import strftime
 class AppMainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        super().__init__()
+
+        # рисование интерфеса
+        self.width = 640
+        self.height = 480
+
+        self.setMinimumSize(QtCore.QSize(self.width, self.height))
+        self.setWindowIcon(QtGui.QIcon('img\\ico1.png'))
+        self.setWindowTitle('Dzetto')
+        self.statusBar().showMessage('for more information check info in main menu')
+
+        main_menu = self.menuBar()
+        file_menu = main_menu.addMenu('File')
+        compare_menu = main_menu.addMenu('compared files')
+
+        choose_dir = QtWidgets.QAction(QtGui.QIcon(None), 'choose_dir', self)
+        choose_dir.setShortcut('Ctrl+Shift+D')
+        choose_dir.triggered.connect(self.choose_directory)
+        file_menu.addAction(choose_dir)
+
+        choose_scan_files = QtWidgets.QAction(QtGui.QIcon(None), 'choose_scan_files', self)
+        choose_scan_files.setShortcut('Ctrl+Shift+F')
+        choose_scan_files.triggered.connect(self.choose_scanned_files)
+        compare_menu.addAction(choose_scan_files)
+
+        scan_files = QtWidgets.QAction(QtGui.QIcon(None), 'scan_files', self)
+        scan_files.setShortcut('Ctrl+Shift+G')
+        scan_files.triggered.connect(self.translate_w)
+        compare_menu.addAction(scan_files)
+
+        exit_button = QtWidgets.QAction(QtGui.QIcon(None), 'exit_button', self)
+        exit_button.setShortcut('Ctrl+Q')
+        exit_button.triggered.connect(app.closeAllWindows)
+        file_menu.addAction(exit_button)
+
+        self.text_box_1 = QtWidgets.QTextEdit(self)
+        self.text_box_1.resize(self.width, self.height)
+        self.text_box_1.move(0, 20)
+
+        # дополнительные параметры
         self.new_file_path = 'E:/proektu/decoder_app/new_files'
         self.new_file_names = []
         self.new_file_directory = []
@@ -23,42 +63,9 @@ class AppMainWindow(QtWidgets.QMainWindow):
         self.result_files_list = []
         self.element = []
 
-        print('def initGUI')
-        main_menu = self.menuBar()
-        file_menu = main_menu.addMenu('File')
-        compare_menu = main_menu.addMenu('compared files')
-
-        choose_dir = QtWidgets.QAction(QtGui.QIcon(None), 'choose_directory', self)
-        choose_dir.setShortcut('Ctrl+Shift+D')
-        choose_dir.triggered.connect(self.choose_directory)
-        file_menu.addAction(choose_dir)
-
-        choose_scan_files = QtWidgets.QAction(QtGui.QIcon(None), 'choose_scan_files', self)
-        choose_scan_files.setShortcut('Ctrl+Shift+F')
-        choose_scan_files.triggered.connect(self.choose_scanned_files)
-        compare_menu.addAction(choose_scan_files)
-
-        scan_files = QtWidgets.QAction(QtGui.QIcon(None), 'scan_files', self)
-        scan_files.setShortcut('Ctrl+Shift+G')
-        scan_files.triggered.connect(self.scan_files_ev)
-        compare_menu.addAction(scan_files)
-
-        exit_button = QtWidgets.QAction(QtGui.QIcon(None), 'Quit', self)
-        exit_button.setShortcut('Ctrl+Q')
-        exit_button.triggered.connect(app.closeAllWindows)
-        file_menu.addAction(exit_button)
-
-        self.width = 120
-        self.height = 480
-
-        self.setMinimumSize(QtCore.QSize(self.width, self.height))
-        self.setWindowIcon(QtGui.QIcon('img\\ico1.png'))
-        self.setWindowTitle('Dzetto')
-
-        self.statusBar().showMessage('for more information check info in main menu')
-        self.text_box_1 = QtWidgets.QTextEdit(self)
-        self.text_box_1.resize(self.width, self.height)
-        self.text_box_1.move(0, 20)
+        # -регистрирование доп окон
+        self.files_to_check = FileButtons(self)
+        self.tr_window = Tr_window(self)
 
     def choose_directory(self):
         print('def choose_directory')
