2415dcf92f5cf8e762ed48130586c1846e407951
yohoko <paradox.wolf@mail.ru>
2019-12-30 23:18:14 +0300

    0.93 переработка описания.

--- file_dilog_window.py
+++ file_dilog_window.py
@@ -18,104 +18,78 @@ class FileDialogWindow(QtWidgets.QDialog):
         self.setMinimumSize(600, 150)
         self.setWindowTitle('Choose files to check')
 
-        self.new_checkboxes = []
-        self.new_list_Label_1 = []
-        self.new_list_Label_2 = []
-
-        self.old_checkboxes = []
-        self.old_list_Label_1 = []
-        self.old_list_Label_2 = []
-
         # строки для тестов
-        # self.new_file_names = ["Checkbox_1", "Checkbox_2", "Checkbox_3", "Checkbox_4", "Checkbox_5"]
-        # self.old_file_names = ["Checkbox_1", "Checkbox_2", "Checkbox_3", "Checkbox_4", "Checkbox_5"]
-
-        # прорисовка меню
-        """
-        я пробовал добавить это в функцию, 
-        но это плохо кончилось 
-        и я решил, что это должно быть в инициализации, 
-        однако никто не говорил, что кноки нельзя добавлять в конец
-        или все же проще было бы создавать новый экземпляр класса диалог...
-        """
-
-        # - присвоение боксов
-        vbox = QtWidgets.QVBoxLayout()
-        hbox = QtWidgets.QHBoxLayout()
-        scrollareawidget = QtWidgets.QWidget()
-        self.grid = QtWidgets.QGridLayout(scrollareawidget)
-        scrl = QtWidgets.QScrollArea()
-        scrl.setWidgetResizable(True)
-
-        b2 = QtWidgets.QPushButton("Accept")
-        b2.clicked.connect(self.accept_results)
+        self.new_file_names = ["Checkbox_1", "Checkbox_2", "Checkbox_3", "Checkbox_4", "Checkbox_5"]
+        self.old_file_names = ["Checkbox_1", "Checkbox_2", "Checkbox_3", "Checkbox_4", "Checkbox_5"]
 
         lb1 = QtWidgets.QLabel(F'Folder for NEW - \n{self.root.new_file_path}')
         lb2 = QtWidgets.QLabel(F'Folder for PLD - \n{self.root.old_file_path}')
 
-        # здесь можно было бы организовать классметод
-        # но мне лень пускай работает, пока работает...
-        for i, v in enumerate(self.new_file_names):
-            self.new_list_Label_1.append('')
-            self.new_checkboxes.append(v)
-            self.new_list_Label_1[i] = QtWidgets.QLabel()
-            self.new_checkboxes[i] = QtWidgets.QCheckBox(v)
-            self.grid.addWidget(self.new_checkboxes[i], i+1, 0)
-            self.grid.addWidget(self.new_list_Label_1[i], i+1, 1)
+        accept_button = QtWidgets.QPushButton("Accept")
+        accept_button.clicked.connect(self.accept_results)
 
-        for i, v in enumerate(self.old_file_names):
-            self.old_list_Label_1.append('')
-            self.old_checkboxes.append(v)
-            self.old_list_Label_1[i] = QtWidgets.QLabel()
-            self.old_checkboxes[i] = QtWidgets.QCheckBox(v)
-            self.grid.addWidget(self.old_checkboxes[i], i+1, 2)
-            self.grid.addWidget(self.old_list_Label_1[i], i+1, 3)
+        v_box = QtWidgets.QVBoxLayout()
+        h_box = QtWidgets.QHBoxLayout()
+        v_box_1 = QtWidgets.QVBoxLayout()
+        v_box_2 = QtWidgets.QVBoxLayout()
 
-        scrl.setWidget(scrollareawidget)
-        hbox.addWidget(lb1)
-        hbox.addWidget(lb2)
-        vbox.addLayout(hbox)
-        vbox.addWidget(scrl)
-        vbox.addWidget(b2)
-        print('setLayout DONE')
-        self.setLayout(vbox)
+        v_box.addLayout(h_box)
+        h_box.addLayout(v_box_1)
+        h_box.addLayout(v_box_2)
+        v_box_1.addWidget(lb1)
+        v_box_2.addWidget(lb2)
+        v_box.addWidget(accept_button)
 
-    # здесь будет рисоваться сетка
-    # def drawing_list(self):
-    #     lb1 = QtWidgets.QLabel(F'Folder for NEW - \n{self.root.new_file_path}')
-    #     lb2 = QtWidgets.QLabel(F'Folder for PLD - \n{self.root.old_file_path}')
+        self.fill(v_box_1, self.new_file_names)
+        self.fill(v_box_2, self.old_file_names)
+
+        self.setLayout(v_box)
+
+    # - присвоение боксов
+    @staticmethod
+    def fill(box, element):
+        for i, v in enumerate(element):
+            box.addWidget(QtWidgets.QRadioButton(text=v))
+    #     for i, v in enumerate(self.old_file_names):
+    #         self.old_list_Label_1.append('')
+    #         self.old_checkboxes.append(v)
+    #         self.old_list_Label_1[i] = QtWidgets.QLabel()
+    #         self.old_checkboxes[i] = QtWidgets.QCheckBox(v)
+    #         self.grid.addWidget(self.old_checkboxes[i], i+1, 2)
+    #         self.grid.addWidget(self.old_list_Label_1[i], i+1, 3)
     #
-    #     # здесь можно было бы организовать классметод
-    #     # но мне лень пускай работает, пока работает...
-    #     for i, v in enumerate(self.new_file_names):
-    #         self.new_list_Label_1.append('')
-    #         self.new_checkboxes.append(v)
-    #         self.new_list_Label_1[i] = QtWidgets.QLabel()
-    #         self.new_checkboxes[i] = QtWidgets.QCheckBox(v)
-    #         self.grid.addWidget(self.new_checkboxes[i], i+1, 0)
-    #         self.grid.addWidget(self.new_list_Label_1[i], i+1, 1)
+    #     scrl.setWidget(scrollareawidget)
+    #     hbox.addWidget(lb1)
+    #     hbox.addWidget(lb2)
+    #     vbox.addLayout(hbox)
+    #     vbox.addWidget(scrl)
+    #     vbox.addWidget(b2)
+    #     print('setLayout DONE')
+    #     self.setLayout(vbox)
 
-    # кнопка подтверждения
+    # # кнопка подтверждения
     def accept_results(self):
-        """
-        здесь вообще творится безумный лес
-        i think i was drunk when wrote this
-        """
-        print('def accept_results')
-        self.new_list_Label_2 = []
-        self.old_list_Label_2 = []
-        self.root.result_dir_list = []
-        self.root.result_files_list = []
-        for i, v in enumerate(self.new_checkboxes):
-            if v.checkState():
-                self.new_list_Label_2.append(self.new_file_names[i])
-                self.root.result_dir_list.append(self.root.new_file_directory[i])
-                self.root.result_files_list.append(self.new_file_names[i])
-
-        for i, v in enumerate(self.old_checkboxes):
-            if v.checkState():
-                self.old_list_Label_2.append(self.old_file_names[i])
-                self.root.result_dir_list.append(self.root.old_file_directory[i])
-                self.root.result_files_list.append(self.old_file_names[i])
-
         self.close()
+
+    #     """
+    #     здесь вообще творится безумный лес
+    #     i think i was drunk when wrote this
+    #     """
+    #     print('def accept_results')
+    #     self.new_list_Label_2 = []
+    #     self.old_list_Label_2 = []
+    #     self.root.result_dir_list = []
+    #     self.root.result_files_list = []
+    #     for i, v in enumerate(self.new_checkboxes):
+    #         if v.checkState():
+    #             self.new_list_Label_2.append(self.new_file_names[i])
+    #             self.root.result_dir_list.append(self.root.new_file_directory[i])
+    #             self.root.result_files_list.append(self.new_file_names[i])
+    #
+    #     for i, v in enumerate(self.old_checkboxes):
+    #         if v.checkState():
+    #             self.old_list_Label_2.append(self.old_file_names[i])
+    #             self.root.result_dir_list.append(self.root.old_file_directory[i])
+    #             self.root.result_files_list.append(self.old_file_names[i])
+    #
+    #     self.hide()
--- main_w.py
+++ main_w.py
@@ -95,21 +95,23 @@ class AppMainWindow(QtWidgets.QMainWindow):
         # проверка на пустые директории
         if self.new_file_path is '' or self.old_file_path is '':
             self.choose_directory()
+            if self.new_file_path is '' or self.old_file_path is '':
+                return
 
         self.new_file_names, self.new_file_directory = self.file_list(self.new_file_path)
         self.old_file_names, self.old_file_directory = self.file_list(self.old_file_path)
 
-        self.locker = False
-
+        self.files_to_check.root = self
         self.files_to_check.exec()
-
-        # после того, как результирующие папки получены
-        # происходит вызов функции,
-        # которая выполняет парсинг,
-        # отправляя в него имена файлов и их положение на пк
-
-        if self.locker is True:
-            print("___________continue")
+        # self.locker = False
+        #
+        # # после того, как результирующие папки получены
+        # # происходит вызов функции,
+        # # которая выполняет парсинг,
+        # # отправляя в него имена файлов и их положение на пк
+        #
+        # if self.locker is True:
+        #     print("___________continue")
 
     @staticmethod
     def file_list(file_path):
@@ -117,8 +119,8 @@ class AppMainWindow(QtWidgets.QMainWindow):
         file_names = []
         file_dir = []
         # r=root, d=directories, f=files
-        for r, d, f in os.walk(file_path):
-            for file in f:
+        for r, d, files in os.walk(file_path):
+            for file in files:
                 if '.yml' in file:
                     file_names.append(file)
                     file_dir.append(r)
@@ -128,72 +130,6 @@ class AppMainWindow(QtWidgets.QMainWindow):
     def translate_w(self):
         print('translate_w')
         self.tr_window.exec()
-
-    # открытие файла
-    def parse_file(self, file_name, raw_str=r'''(?P<key>^[A-Za-z._0-9]*):(?P<value>.*["]*)''', file_encoding="utf_8"):
-        print(f'\n\n{"-_" *5}',
-              '\nfile_name: ', file_name,
-              '\nraw_str: ', raw_str,
-              '\nencoding: ', file_encoding,
-              '\nfile: \n')
-
-        with open(file_name, "r", encoding=file_encoding) as stream:
-            match_str = stream.read()
-            parse_result = re.findall(raw_str, match_str, re.MULTILINE)
-        self.element.append(parse_result)
-        return parse_result
-
-    # сравнение файлов
-    @staticmethod
-    def text_compare(b, d):
-        filename = f'result{strftime("%H_%M")}.yml'
-        with open(filename, 'w', encoding="utf-8-sig") as result_file:
-            result_file.write('l_russian:\n')
-            l_1 = []
-            l_2 = []
-            # сортировка старых ключей
-            for i_1 in b:
-                l_1.append(i_1[0])
-            # сортировка новых ключей
-            for i_2 in d:
-                l_2.append(i_2[0])
-
-            # поиск страых существующих ключей
-            for m, n in enumerate(l_1):
-                if n in l_2:
-                    result_file.write(f'{b[m][0]}:{b[m][1]}\n')
-                    print('старые живые', n)
-            result_file.write(f'\n###НОЫЕ_СТРОКИ###\n\n')
-
-            # поиск новых существующих ключей среди старых
-            for j, i in enumerate(l_2):
-                if i not in l_1:
-                    result_file.write(f'{d[j][0]}:{d[j][1]}\n')
-                    print('абсолютно новые', i)
-
-        # перепись кодировки с винды на линку
-        # мб и не нужно, но пускай будет
-        windows_line_ending = b'\r\n'
-        unix_line_ending = b'\n'
-        with open(filename, 'rb') as file_data:
-            file_text = file_data.read()
-        file_text = file_text.replace(windows_line_ending, unix_line_ending)
-        with open(filename, 'wb') as file_data:
-            file_data.write(file_text)
-
-    # собственно вызывает парсинг файлов
-    def text_parse(self):
-        """
-        получение имен файлов
-        """
-        print('text_parse')
-        a = input('Введите имя старого файла (с расширением): ')
-        c = input('Введите имя нового файла (с расширением): ')
-        b = self.parse_file(file_name=f'{os.path.dirname(__file__)}/{a}.yml')
-        d = self.parse_file(file_name=f'{os.path.dirname(__file__)}/{c}.yml')
-        print('\n\nрабочий каталог: ', os.path.abspath(__file__))
-        self.text_compare(b, d)
-        return print("\nFINISHED")
 
     # подтверждение выхода из программы
     def closeEvent(self, event):
