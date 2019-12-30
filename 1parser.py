6c9374e181e94279c601658abde51f56c4138922
Alexy_Yohko <47605546+olexapro@users.noreply.github.com>
2019-12-30 16:36:41 +0300

    Update 1parser.py

--- 1parser.py
+++ 1parser.py
@@ -2,8 +2,8 @@ import re
 
 def parse_file(file_name):
     # preinstalls
-    raw_str=r'''(?P<key>[ ]*[A-Za-z._0-9]*):(?P<value>.+["]*)'''
-    file_encoding="utf_8"):
+    raw_str=r'''(?P<key>[ ]*[A-Za-z._0-9]*):(?P<value>.+)'''
+    file_encoding="utf_8"
     print('\nfile_name: ', file_name,
           '\nraw_str: ', raw_str,
           '\nencoding: ', file_encoding,
@@ -16,6 +16,6 @@ def parse_file(file_name):
         for i in parse_result:
             cotege[i[0]] = i[1]
             #print(str(i[0]) + ':' + cotege[i[0]])
-    return parse_result
+    return cotege
     
 #print(parse_file(r'C:\Users\KIP\Desktop\my_projects\dzettoapp\old\old.yml'))
